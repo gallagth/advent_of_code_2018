c1d467490bfaf3314932644c8b436c181c64a37f
Thomas Gallagher <thomas.gallagher@booking.com>
2018-12-29 15:25:52 +0100

    Day 1 part 2 done

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,22 +1,25 @@
-use std::env;
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use std::collections::HashMap;
 
-
-fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-
+fn main() {
     // Part 1
-    let file = File::open(filename)?;
-    let mut freq: i64 = 0;
-    for line in BufReader::new(file).lines() {
-        let freq_shift = line?.parse::<i64>().unwrap();
-        freq += freq_shift;
-    }
-    println!("{}", freq);
+    let puzzle = include_str!("input.txt");
+    let values: Vec<i64> = puzzle.lines().map(|x| x.parse::<i64>().unwrap()).collect();
+    let freq_sum: i64 = values.iter().sum();
+    println!("{}", freq_sum);
 
     // Part 2
-
-    Ok(())
+    let mut frequencies = HashMap::new();
+    let mut current_freq = 0;
+    let mut found = false;
+    while !found {
+        for v in values.iter() {
+            frequencies.insert(current_freq, true);
+            current_freq += v;
+            if frequencies.contains_key(&current_freq) {
+                found = true;
+                break;
+            }
+        }
+    }
+    println!("{:?}", current_freq);
 }
