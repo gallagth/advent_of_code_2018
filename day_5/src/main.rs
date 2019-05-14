4aced2c680e8cf54b0f2653d383e9c0257b76fbe
Thomas Gallagher <thomas.gallagher@booking.com>
2019-05-14 16:51:52 +0200

    Day 5 part 2 done

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::VecDeque;
+use std::iter::FromIterator;
 
 fn are_opposites(c1: char, c2: char) -> bool {
     (c1 as i32 - c2 as i32).abs() == 0x20
@@ -25,4 +26,14 @@ fn main() {
     let puzzle = include_str!("input.txt").trim();
     println!("{:?}", puzzle.len());
     println!("{:?}", collapse(puzzle));
+    let mut min = puzzle.len();
+    for c in b'a'..b'z' {
+        let upper_c = (c as char).to_uppercase().next().unwrap();
+        let filtered: Vec<char> = puzzle.chars().filter(|&l| l != c as char && l != upper_c).collect();
+        let collapsed_len = collapse(&String::from_iter(filtered));
+        if collapsed_len < min {
+            min = collapsed_len;
+        }
+    }
+    println!("{:?}", min);
 }
